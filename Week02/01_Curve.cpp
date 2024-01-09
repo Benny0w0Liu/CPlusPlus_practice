5ba5546dc2d7ef1b29c668a2f539005309f547bf
劉邦均 <bennyliu9005@gmail.com>
2024-01-10 01:03:24 +0800

    Fix Long Method

--- Week02/01_Curve.cpp
+++ Week02/01_Curve.cpp
@@ -9,69 +9,26 @@ int main(){
 		cin>>user[i];
 	}
 	int arr[n][n];
-	for(int i=n;i>0;i--){
-		if(i!=n){
-			for(int j=0;j<i;j++){
-				switch(direction%4){
-					case 0:
-						x++;
-						break;
-					case 1:
-						y++;
-						break;
-					case 2:
-						x--;
-						break;
-					case 3:
-						y--;
-						break;
-				}
-				arr[y][x]=user[count];
-				count++;
+	int i=0, j=0, di=0, dj=1;
+    for (int x=0; x<n*n; x++) {
+        arr[i][j] = user[x];
+        if (i+di<0 || i+di>=n || j+dj<0 || j+dj>=n || arr[i+di][j+dj]!=0) {
+            if(di== 0 && dj== 1){
+				di= 1;
+				dj= 0;
+			}else if(di== 1 && dj== 0){
+				di= 0; 
+				dj=-1;
+			}else if(di== 0 && dj==-1){
+				di=-1; 
+				dj= 0;
+			}else if(di==-1 && dj== 0) {
+				di= 0; 
+				dj= 1;
 			}
-			direction++;
-			for(int j=0;j<i;j++){
-				
-				switch(direction%4){
-					case 0:
-						x++;
-						break;
-					case 1:
-						y++;
-						break;
-					case 2:
-						x--;
-						break;
-					case 3:
-						y--;
-						break;
-				}
-				arr[y][x]=user[count];
-				count++;
-			} 
-			direction++;
-		}else{
-			for(int j=0;j<i;j++){				
-				switch(direction%4){
-					case 0:
-						x++;
-						break;
-					case 1:
-						y++;
-						break;
-					case 2:
-						x--;
-						break;
-					case 3:
-						y--;
-						break;
-				}
-				arr[y][x]=user[count];
-				count++;
-			}
-			direction++;
-		}
-	}
+        }
+        i+=di; j+=dj;
+    }
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
 			cout<<setw(3)<<arr[i][j];
